82633b86aad827d245432ea38e7c85ce9b803af8
Mahendra <mahendrabrowny@gmail.com>
2022-12-28 19:55:02 +0530

    remove logs from client side

--- client/src/components/common/MediaVideosSlide.jsx
+++ client/src/components/common/MediaVideosSlide.jsx
@@ -8,7 +8,6 @@ const MediaVideo = ({ video }) => {
   const iframeRef = useRef();
 
   useEffect(() => {
-    console.log("ifram");
     const height = iframeRef.current.offsetWidth * 9 / 16 + "px";
     iframeRef.current.setAttribute("height", height);
   }, [video]);
